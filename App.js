55c1abde5e436ade20b9107910210e2011a17f98
A Richardson <g.andrerichards@gmail.com>
2020-01-16 02:32:57 -0500

    add input and list data

--- App.js
+++ App.js
@@ -1,21 +1,62 @@
 import React, { useState } from 'react'
-import { StyleSheet, Text, View, Button } from 'react-native'
+import { StyleSheet, Text, View, Button, TextInput, ScrollView } from 'react-native'
 
 export default function App() {
-  const [outputText, setOutputText] = useState('Open up App.js to start working on your app!')
+  const [enteredGoal, setEnteredGoal] = useState('')
+  const [courseGoals, setCourseGoals] = useState([])
+
+  const goalInputHandler = text => {
+    setEnteredGoal(text)
+  }
+
+  const addGoalHandler = () => {
+    setCourseGoals(courseGoals => [...courseGoals, enteredGoal])
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>{outputText}</Text>
-      <Button title="Change Text" onPress={() => setOutputText('Changed Text')} />
+    <View style={styles.screen}>
+      <View style={styles.viewContainer}>
+        <TextInput
+          placeholder="Course Goal"
+          style={styles.input}
+          onChangeText={goalInputHandler}
+          value={enteredGoal} />
+        <Button title="ADD" onPress={addGoalHandler} />
+      </View>
+      <ScrollView>
+        {courseGoals.map(goal => <View style={styles.listItem} key={goal}>
+          <Text>{goal}</Text>
+        </View>)}
+      </ScrollView>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  container: {
+  screen: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingVertical: 40,
+    paddingHorizontal: 15
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
+  viewContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  listItem: {
+    padding: 10,
+    marginBottom: 5,
+    backgroundColor: '#eee',
+    borderColor: 'black',
+    borderWidth: 1
+  },
+  input: {
+    borderColor: 'black',
+    borderBottomWidth: 1,
+    padding: 5,
+    width: '80%'
+  }
 })
